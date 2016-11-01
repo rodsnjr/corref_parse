a3c948328ba7bae651b3d69678bdcd76ccde06c2
RODNEY SALES NOGUEIRA JR <16190172@l3043072.portoalegre.pucrsnet.br>
2016-11-01 13:34:18 -0200

    adicionando ws pra rodar no heroku

--- main.js
+++ main.js
@@ -4,6 +4,24 @@ var fs = require('fs')
   , xmlStream = flow(inFile)
 ;
 
-xmlStream.on('tag:Cadeias', function(cadeias) {
-  console.log(cadeias);
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  xmlStream.on('tag:Cadeias', function(cadeias) {
+    response.send(cadeias);
+  });
 });
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
